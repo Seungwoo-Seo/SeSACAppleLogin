94ab0c1a3923eebae9ce64cf494f58b34c0ab7c4
SeungwooSeo <prodvssw.98@gmail.com>
2023-12-28 10:11:44 +0900

    Initial Commit

---
+++ SeSACAppleLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACAppleLogin
+//
+//  Created by 서승우 on 2023/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
